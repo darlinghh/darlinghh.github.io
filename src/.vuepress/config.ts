2d3420b07a5fb3cd3c906c141cc89f9ed6e0dbdd
admin <wangwulliuqibajiu@gmail.com>
2024-01-20 13:24:04 +0800

    基本配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,14 +2,14 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+    base: "/",
 
-  lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
+    lang: "zh-CN",
+    title: "技术博客",
+    description: "vuepress-theme-hope 的文档演示",
 
-  theme,
+    theme,
 
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
+    // 和 PWA 一起启用
+    // shouldPrefetch: false,
 });
--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,30 +1,12 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  "/demo/",
-  {
-    text: "指南",
-    icon: "lightbulb",
-    prefix: "/guide/",
-    children: [
-      {
-        text: "Bar",
+    "/",
+    {
+        text: "前端导航",
+        link: "/navigation",
         icon: "lightbulb",
-        prefix: "bar/",
-        children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
-      },
-      {
-        text: "Foo",
-        icon: "lightbulb",
-        prefix: "foo/",
-        children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
-      },
-    ],
-  },
-  {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
+        // 仅在 `/zh/guide/` 激活
+        // activeMatch: "^/zh/guide/$",
+    },
 ]);
